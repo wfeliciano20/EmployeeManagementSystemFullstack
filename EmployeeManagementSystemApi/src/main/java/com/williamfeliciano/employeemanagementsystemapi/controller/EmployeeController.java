4b06041ccd7b6b7889932fd7118f58192279ce93
wfeliciano20 <william.feliciano2@gmail.com>
2022-08-23 20:08:02 -0400

    updated cross origin

--- EmployeeManagementSystemApi/src/main/java/com/williamfeliciano/employeemanagementsystemapi/controller/EmployeeController.java
+++ EmployeeManagementSystemApi/src/main/java/com/williamfeliciano/employeemanagementsystemapi/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3001", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
